bd6d41872e8ce7483d4fd59856606753a99e4817
Muhammad Sajid <muhammadsajidkhan616@gmail.com>
2022-02-15 23:29:18 +0500

    basic layout done

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,17 @@
-console.log(5);
-console.log(5);
+const time = document.querySelector(".notify__time");
+
+setInterval(() => {
+  let localTime = new Date().toLocaleTimeString();
+  time.textContent = localTime;
+}, 1000);
+
+// ==================================
+const allApps = document.querySelectorAll("main>*");
+const allIcons = document.querySelectorAll(".apps>*");
+console.log(allIcons);
+allIcons.forEach((icon) => {
+  icon.addEventListener("click", (e) => {
+    icon.classList.remove("active");
+    console.log(e.target.classList);
+  });
+});
